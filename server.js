bd2c171e24708e1e5ee5bb2ca9b8ac691aebc696
KSB21ST <21superbaby@gmail.com>
2021-11-06 15:33:02 +0900

    modify user when same name in login in join

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { User } = require("./models/user");
 const cors = require('cors');
-const nms = require('./streaming');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -45,7 +44,8 @@ app.post("/api/user/register", async(req, res) => {
         return res.json({ success: false, err });
       }else {
         if(sameUser != null){ //이미 같은 유저가 디비에 있을떄
-          return res.status(200).json(
+          console.log("same user");
+          return res.json(
           {
             userName: user.userName,
             action: user.action,
@@ -73,6 +73,63 @@ app.post("/api/user/register", async(req, res) => {
   });
 });
 
+app.post("/api/user/modifyuser", async(req, res) => {
+  res.set('Access-Control-Allow-Credentials', 'true');
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+
+  const user = new User(req.body);
+  const _user = new User(req.body);
+  var userList = mongoose.model('User');
+  if(user.userName == ""){
+    return res.status(200).json({ success: true });
+  }
+  // userList.findOne({userName: user.userName}, function(err, sameUser){
+  //     if(err){
+  //       return res.json({ success: false, err });
+  //     }else {
+  //       if(sameUser != null){ //이미 같은 유저가 디비에 있을떄
+  //         console.log("same user");
+  //         user.save((err, userInfo) => {
+  //           if (err) return res.json({ success: false, err });
+  //           return res.status(200).json(
+  //           {
+  //             userName: user.userName,
+  //             action: user.action,
+  //             character: user.character,
+  //             emogee : user.emogee,
+  //             position_x: user.position_x,
+  //             position_y : user.position_y,
+  //             team: user.team
+  //           });
+  //         });
+  //       }else{
+  //         return res.json({ success: false, err });
+  //       }
+  //     }
+  //   })
+  userList.findOneAndUpdate(
+    {userName: user.userName},
+    {
+      userName: user.userName,
+        action: user.action,
+        character: user.character,
+        emogee : user.emogee,
+        position_x: user.position_x,
+        position_y : user.position_y,
+        team: user.team
+    });
+    return res.json(
+    {
+      userName: user.userName,
+      action: user.action,
+      character: user.character,
+      emogee : user.emogee,
+      position_x: user.position_x,
+      position_y : user.position_y,
+      team: user.team
+    });
+});
+
 app.get("/api/user/users", async(req, res) => {
   res.set('Access-Control-Allow-Credentials', 'true');
   res.header("Access-Control-Allow-Origin", req.headers.origin);
@@ -83,4 +140,29 @@ app.get("/api/user/users", async(req, res) => {
 
 app.use(cors({credentials: true, origin: 'http://localhost:3002'}));
 app.listen(port, () => console.log(`listening on port ${port}`));
-nms.run();
+
+//for streaming
+// const nms = require('./streaming');
+// nms.run();
+
+//for socket io
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:3002",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connection");
+  socket.on("init", (payload) => {
+    console.log(payload);
+  });
+  socket.on('msg-snd', item => {
+    console.log('(server) sended from ' + item.name + ': [ ' + item.message + ' ]');
+    io.emit('msg-rcv', {name: item.name, message: item.message});
+  });
+});
+
+httpServer.listen(80);
